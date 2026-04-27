afdc7259ca9ccdbcb0b71d69ab03818d53be9c0a
1994-spa <spacersytb@gmail.com>
2026-04-27 18:13:28 +0200

    git add index.html

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Supabase Configuration
 // Remplacez ces valeurs par vos vraies clés Supabase
 const SUPABASE_CONFIG = {
-  url: 'https://xphuolvbamdkizydveij.supabase.co/rest/v1/',
+  url: 'https://xphuolvbamdkizydveij.supabase.co',
   anonKey: 'YOUReyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhwaHVvbHZiYW1ka2l6eWR2ZWlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwNDQ2MDksImV4cCI6MjA5MjYyMDYwOX0.vpxV6ApwAkhlTx-LMCBZwee7LCdQGYP_5sbPMXEFFUQ'
 };
 
